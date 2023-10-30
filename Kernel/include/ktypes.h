1dce460f973c4ac098a7dac030da90e1b40579e7
MINAqwq <84045544+MINAqwq@users.noreply.github.com>
2023-10-30 01:36:48 +0100

    make signed types explicit

--- Kernel/include/ktypes.h
+++ Kernel/include/ktypes.h
@@ -10,9 +10,9 @@ typedef unsigned long uint64;
 
 typedef unsigned int uint;
 
-typedef char int8;
-typedef short int16;
-typedef int int32;
-typedef long int64;
+typedef signed char int8;
+typedef signed short int16;
+typedef signed int int32;
+typedef signed long int64;
 
 #endif

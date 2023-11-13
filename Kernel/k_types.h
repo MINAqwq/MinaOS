37dab9901abe2ba4c56eb4b03d2d718760a3927a
MINAqwq <84045544+MINAqwq@users.noreply.github.com>
2023-11-13 20:46:58 +0100

    kernel type update

--- Kernel/k_types.h
+++ Kernel/k_types.h
@@ -3,16 +3,19 @@
 #ifndef KERNEL_TYPES_H
 #define KERNEL_TYPES_H
 
-typedef unsigned char uint8;
+typedef unsigned char  uint8;
 typedef unsigned short uint16;
-typedef unsigned int uint32;
-typedef unsigned long uint64;
+typedef unsigned int   uint32;
+typedef unsigned long  uint64;
 
 typedef unsigned int uint;
+typedef unsigned char bool;
 
-typedef signed char int8;
+typedef signed char  int8;
 typedef signed short int16;
-typedef signed int int32;
-typedef signed long int64;
+typedef signed int   int32;
+typedef signed long  int64;
+
+typedef char *string;
 
 #endif
